5325392ebdd0a7cf8484885bac46558ae329ac3e
tttalhaa <78103636+tttalhaa@users.noreply.github.com>
2022-07-26 04:27:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client as Bot
+
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+Client = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="")
+)
+
+Client.run() 
